b2066ffd813ec6a4042e186509428e396b4eaff2
Kenji <rllynwatme3@gmail.com>
2024-12-01 17:19:16 +0500

    nunu

--- lib/pool.ts
+++ lib/pool.ts
@@ -7,7 +7,7 @@ export const getPool = () => {
     pool = mysql.createPool({
       host: process.env.DB_HOST || "localhost",
       user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "",
+      password: process.env.DB_PASSWORD || "goodfella6",
       database: process.env.DB_NAME || "ani_track",
       waitForConnections: true,
       connectionLimit: 10,
